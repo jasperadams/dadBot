11d80bc7a0b1e765ebc50fff925de84e28e0ca34
jasperadams <jasperadams@students.berkeley.net>
2018-05-18 10:33:45 -0700

    hhi

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-var questionNum = 0;													// keep count of question, used for IF condition.
-var question = '<h1>what is your name?</h1>';				  // first question
 
-var output = document.getElementById('output');				// store id="output" in output variable
-output.innerHTML = question;													// ouput first question
+var questionNum = 0;
+var question = '<h1>what is your name?</h1>';
+
+var output = document.getElementById('output');
+
+$(document).ready(function() {
+    console.log(output);
+    output.innerHTML = question;
+});
 
 function bot() {
     var input = document.getElementById("input").value;
     console.log(input);
 
     if (questionNum == 0) {
-        output.innerHTML = '<h1>hello ' + input + '</h1>';// output response
-        document.getElementById("input").value = "";   		// clear text box
-        question = '<h1>how old are you?</h1>';			    	// load next question
-        setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
+        output.innerHTML = '<h1>hello ' + input + '</h1>';
+        document.getElementById("input").value = "";
+        question = '<h1>how old are you?</h1>';
+        setTimeout(timedQuestion, 2000);
     }
 
     else if (questionNum == 1) {
@@ -27,10 +32,9 @@ function timedQuestion() {
     output.innerHTML = question;
 }
 
-//push enter key (using jquery), to run bot function.
 $(document).keypress(function(e) {
     if (e.which == 13) {
-        bot();																						// run bot function when enter key pressed
-        questionNum++;																		// increase questionNum count by 1
+        bot();
+        questionNum++;
     }
 });
