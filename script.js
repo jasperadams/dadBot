4dff5eb74824805ef9b98d3fdcc8cf51417ecc8c
jasperadams <jasperadams@students.berkeley.net>
2018-05-22 10:34:09 -0700

    update

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 
-var questionNum = 0;
+var qNum = 0;
 var question = '<h1>what is your name?</h1>';
 
 var output = document.getElementById('output');
 
 $(document).ready(function() {
     console.log(output);
-    output.innerHTML = question;
+    $("#output").html(question);
 });
 
-function bot() {
-    var input = document.getElementById("input").value;
+function chat() {
+    var input = $("#input").val();
     console.log(input);
 
-    if (questionNum == 0) {
-        output.innerHTML = '<h1>hello ' + input + '</h1>';
-        document.getElementById("input").value = "";
+    if (qNum === 0) {
+        $("#output").html('<h1>hello ' + input + '</h1>');
+        $('#input').val("");
         question = '<h1>how old are you?</h1>';
         setTimeout(timedQuestion, 2000);
     }
 
-    else if (questionNum == 1) {
-        output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
-        document.getElementById("input").value = "";
+    else if (qNum === 1) {
+        $("#output").html('<h1>so you were born in ' + (2018 - input) + '</h1>');
+        $('#input').val("");
+        question = '<h1>where are you from?</h1>';
+        setTimeout(timedQuestion, 2000);
+    }
+
+    else if (qNum === 2) {
+        $("#output").html('<h1>so you were born in ' + (2018 - input) + '</h1>');
+        $('#input').val("");
         question = '<h1>where are you from?</h1>';
         setTimeout(timedQuestion, 2000);
     }
 }
 
 function timedQuestion() {
-    output.innerHTML = question;
+    $("#output").html(question);
 }
 
 $(document).keypress(function(e) {
-    if (e.which == 13) {
-        bot();
-        questionNum++;
+    if (e.which === 13) {
+        chat();
+        qNum++;
     }
 });
