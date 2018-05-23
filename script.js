ef23d3598e15fc65335153cc3300db7c8d841cc3
jasperadams <jasperadams@students.berkeley.net>
2018-05-23 09:45:11 -0700

    update

--- script.js
+++ script.js
@@ -5,32 +5,31 @@ var question = '<h1>what is your name?</h1>';
 var output = document.getElementById('output');
 
 $(document).ready(function() {
-    console.log(output);
     $("#output").html(question);
 });
 
 function chat() {
-    var input = $("#input").val();
+    var input = $("#input ").val();
     console.log(input);
 
     if (qNum === 0) {
         $("#output").html('<h1>hello ' + input + '</h1>');
         $('#input').val("");
-        question = '<h1>how old are you?</h1>';
+        question = '<h1>how much of your last assignment did you do?</h1>';
         setTimeout(timedQuestion, 2000);
     }
 
     else if (qNum === 1) {
-        $("#output").html('<h1>so you were born in ' + (2018 - input) + '</h1>');
+        $("#output").html('<h1>' + input + '? ok that is a 14/15 </h1>');
         $('#input').val("");
-        question = '<h1>where are you from?</h1>';
+        question = '<h1>what is your favorite type of meme?</h1>';
         setTimeout(timedQuestion, 2000);
     }
 
     else if (qNum === 2) {
-        $("#output").html('<h1>so you were born in ' + (2018 - input) + '</h1>');
+        $("#output").html('<h1> i guess ' + input + ' are cool .. but i like Albinson memes</h1>');
         $('#input').val("");
-        question = '<h1>where are you from?</h1>';
+        question = '<h1></h1>';
         setTimeout(timedQuestion, 2000);
     }
 }
