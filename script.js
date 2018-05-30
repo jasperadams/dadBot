2ea4239959783028eb80ff22927ec98eae08a60c
jasperadams <32275461+jasperadams@users.noreply.github.com>
2018-05-29 22:05:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,98 +34,41 @@ function chat() {
         $("#output").html('<h1>' + input + '? FACT: bears eat beets. bears, beets, Battlestar Galactica.</h1>');
         $('#img').attr('src', 'https://i1.sndcdn.com/artworks-000001104797-rgzcts-t500x500.jpg');
         $('#input').val("");
-        question = '<h1>anyway, guess how old I am! </h1>';
+        question = '<h1>anyway... i heard today is a special day - is that right?</h1>';
         setTimeout(timedImage, 5000);
         setTimeout(timedQuestion, 5000);
     }
 
     else if (qNum === 3) {
-        $("#output").html('<h1>' + input +' ? </h1>');
+        if(input.toLowerCase().includes("yes")){
+            $("#output").html('<h1>youre right! today is the day I was born - literally!</h1>');
+        }
+        else if(input.toLowerCase().includes("no")){
+            $("#output").html('<h1>i think youre wrong - today is the day I was born - literally!</h1>');
+        } else {
+            $("#output").html('<h1>' + input + '? well today is the day I was born - literally!</h1>');
+        }
         $('#input').val("");
-        question = '<h1>have you seen otis recently?</h1>';
+        question = '<h1>but I think its also special for a different reason ... whats the date today?</h1>';
         setTimeout(timedQuestion, 3000);
     }
 
     else if (qNum === 4) {
-        console.log(input);
-        if(input.toLowerCase().includes("yes")){
-            $("#output").html('<h1>really? then you must have been JUULing with him in the bathroom</h1>');
-        }
-        else if(input.toLowerCase().includes("no")){
-            $("#output").html('<h1>me neither - he is probably JUULing in the bathroom</h1>');
+        if(input.toLowerCase().includes("may") && input.toLowerCase().includes("30") ){
+            $("#output").html('<h1>' + input + '? well then ... </h1>');
         } else {
-            $("#output").html('<h1>well he is defintely vaping</h1>');
+            $("#output").html('<h1> oh well I guess I was wrong ... nevermind</h1>');
         }
         $('#input').val("");
-        question = '<h1>who is the worst teacher at berkeley high?</h1>';
-        setTimeout(timedQuestion, 4000);
+        question = '<h1>press enter!</h1>';
+        setTimeout(timedQuestion, 2000);
     }
 
     else if (qNum === 5) {
-        if (input.toLowerCase().includes("holston")) {
-            $("#output").html('<h1>yes, mr holston is terrible</h1>');
-        } else if(input.toLowerCase().includes("you")) {
-            $("#output").html('<h1>no, mr holston is definetly worse than me</h1>');
-        } else {
-            $("#output").html('<h1>you think ' + input + ' is the worst? nah mr holston is</h1>');
-        }
+        $("#output").html('<h1></h1>');
+        $('#img').attr('src', 'img/birthay.png');
         $('#input').val("");
-        question = '<h1>what is your favorite website?</h1>';
-        setTimeout(timedQuestion, 3500);
-    }
-
-    else if (qNum === 6) {
-        if(input.toLowerCase() == "cornhub") {
-            $("#output").html('<h1>ok here you go: </h1><iframe src="https://cornhubpremium.blog/" height="500" width="500"></iframe>');
-        } else {
-            $("#output").html('<h1>' + input + '? really? I like this: </h1><iframe src="https://cornhubpremium.blog/" height="500" width="500"></iframe>');
-        }
-        $('#input').val("");
-        question = '<h1>want to see my backend?</h1>';
-        setTimeout(timedQuestion, 5000);
-    }
-
-    else if (qNum === 7) {
-        if(input.toLowerCase() == "yes"){
-            $("#output").html('<h1>here you go lol</h1>');
-            $('#img').attr('src', 'img/back.png');
-        }
-        else if(input.toLowerCase() == "no"){
-            $("#output").html('<h1>here you go anyway lol</h1>');
-            $('#img').attr('src', 'img/back.png');
-
-        } else {
-            $("#output").html('<h1>well here you go</h1>');
-            $('#img').attr('src', 'img/back.png');
-        }
-        $('#input').val("");
-        question = '<h1>want to see some memes?</h1>';
-        setTimeout(timedImage, 3000);
-        setTimeout(timedQuestion, 3000);
-    }
-
-    else if (qNum === 8) {
-        if(input.toLowerCase() == "yes"){
-            $("#output").html('<h1>ok, then press enter</h1>');
-        }
-        else if(input.toLowerCase() == "no"){
-            $("#output").html('<h1>really? i think you do ... press enter</h1>');
-        } else {
-            $("#output").html('<h1>press enter</h1>');
-        }        
-        $('#input').val("");
-    }
-
-    else if (qNum === 9) {
-        var num = Math.floor(Math.random() * 26) + 14;
-        var img = '<img src="meme/ALBY%20(' + num + ').jpg" width=428 height="240">';
-        console.log(img);
-        $("#output").html(img);
-        $('#input').val("");
-        qNum = 8;
-    }
-
-}
+    } 
 
 function timedQuestion() {
     $("#output").html(question);
